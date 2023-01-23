06bcb2cfc682850b07e98f447456891a3e8984a1
Federico Jimenez <ricojimenez89@gmail.com>
2023-01-23 01:19:49 -0500

    added REST methods

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const axios = require('axios')
 const express = require('express');
 const {Questions, Users} = require('./db')
 const Dev = require('../ds');
@@ -15,17 +16,30 @@ app.use(express.urlencoded({ extended: true })); // Parses url
 app.use(express.static(distPath)); // Statically serve up client directory
 app.use(Dev()); // Dev middleware - do not move
 
+// get users
 app.get('/api/users', (req, res) => {
-});
+  Users.find()
+    .then(() => res.sendStatus(200))
+    .catch(() => res.sendStatus(404))
+})
 
+// create user
 app.post('/api/users', (req, res) => {
   const {body} = req;
   Users.create(body)
-    .then(data => {
-      console.log(data)
-      res.status(201).json(data)
-    })
+    .then(data => res.status(201).json(data))
     .catch(() => res.sendStatus(500))
+})
+
+// update score
+app.put('/api/users/:_id', (req, res) => {
+  const {params: {id}, body: {score}} = req
+  Users.updateOne({id}, {score})
+    .then((result) => {
+      console.log(result)
+      res.sendStatus(200)
+    })
+    .catch(() => res.sendStatus(400))
 })
 
 /** Place all code above here */
