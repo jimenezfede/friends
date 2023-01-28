df7c36c15cf0b56224c3e62458b7a148e431de79
Federico Jimenez <ricojimenez89@gmail.com>
2023-01-28 18:44:07 -0500

    removed unused mongoose and added netlify file

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,5 @@
 import path from 'path'
 import express, {Request, Response} from 'express'
-const {Users} = require('./db')
 const Dev = require('../ds');
 
 const port = 4000;
@@ -14,29 +13,6 @@ app.use(express.json()); // Parse the request body
 app.use(express.urlencoded({ extended: true })); // Parses url
 app.use(express.static(distPath)); // Statically serve up client directory
 app.use(Dev()); // Dev middleware - do not move
-
-// get users
-app.get('/api/users', (req: Request, res: Response) => {
-  Users.find()
-    .then(() => res.sendStatus(200))
-    .catch(() => res.sendStatus(404))
-})
-
-// create user
-app.post('/api/users', (req, res) => {
-  const {body: {name, score}} = req;
-  Users.updateOne({name}, {name, score}, {upsert: true})
-    .then((data: any) => res.status(201).json(data))
-    .catch(() => res.sendStatus(500))
-})
-
-// update score
-app.put('/api/users/:_name', (req, res) => {
-  const {params: {_name}, body: {score}} = req
-  Users.updateOne({_name}, {score})
-    .then((result: any) => res.sendStatus(200))
-    .catch(() => res.sendStatus(400))
-})
 
 /** Place all code above here */
 app.listen(port, () => {
